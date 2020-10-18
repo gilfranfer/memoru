bc793aa142ab9fb0cbe03e27f07c021e3091926e
Francisco Gil <franfer@Franfers-MacPro.attlocal.net>
2020-10-18 15:31:29 -0500

    Header Nav

--- js/app/setup.js
+++ js/app/setup.js
@@ -1,1 +1,14 @@
 var memoruApp = angular.module('memoruApp',[]);
+
+//Set some constants on the Angular root scope
+memoruApp.run(function($rootScope) {
+	$rootScope.appConstants = {
+        app:{name:"Memoru", version: "1.0"}
+    };
+
+    $rootScope.i18n = {
+        menu:{login:"Log in", logout: "Log out", register:"Register"},
+        labels:{tasks:"Tasks"}
+    };
+
+});
