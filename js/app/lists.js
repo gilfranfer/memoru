10c39e896602bb27b19df5f554bf423829c9cdde
Francisco Gil <franfer@Franfers-MacPro.attlocal.net>
2020-10-19 07:58:12 -0500

    Introducing Lists Controller and Service

---
+++ js/app/lists.js
@@ -0,0 +1,29 @@
+//This fila contains Controllers and Services related to List
+
+memoruApp.controller('ListsCtrl',
+	['$rootScope','$scope','$firebaseAuth','$location',
+    function($rootScope,$scope,$firebaseAuth,$location){
+
+        $scope.createDefaultLists = function(){
+            let userId = constants.test.userID;
+            let usersFolder = constants.db.folders.users;
+
+            var postData = {
+                author: "username",
+                uid: "uid",
+                body: "body"
+              };
+            firebase.database().ref(usersFolder.child(userId)).update({
+                postData 
+            });
+        };
+
+    }]
+);
+
+memoruApp.factory('ListsSvc',
+    ['$rootScope', '$firebaseArray', '$firebaseObject',
+	function($rootScope, $firebaseArray, $firebaseObject){
+
+    }]
+);
