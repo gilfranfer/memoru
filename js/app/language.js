5b2ac6a7423c7d6b5b5ec27f1168ecb04e4f52b9
Francisco Gil <franfer@Franfers-MacPro.attlocal.net>
2020-10-19 08:07:39 -0500

    Language js file

---
+++ js/app/language.js
@@ -0,0 +1,10 @@
+const languages = {
+    english: {
+        menu:{login:"Log in", logout: "Log out", register:"Register"},
+        labels:{tasks:"Tasks"}
+    },
+    spanish: {
+        menu:{login:"Iniciar sesión", logout: "Cerrar sesión", register:"Registro"},
+        labels:{tasks:"Tareas"}
+    }
+};
