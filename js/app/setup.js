df7252d259dfbd7c370c8b3b556f1a15b8c22ce8
Francisco Gil <franfer@Franfers-MacPro.attlocal.net>
2020-10-19 07:57:28 -0500

    run fucntion on angular app

--- js/app/setup.js
+++ js/app/setup.js
@@ -1,14 +1,38 @@
-var memoruApp = angular.module('memoruApp',[]);
+/* Initialize database connection using the firebaseConfig defined direclty in netlify host */
+var memoruFire = firebase.initializeApp(firebaseConfig); 
+var memoruAngular = angular.module('memoruApp',['ngRoute','firebase']);
 
-//Set some constants on the Angular root scope
-memoruApp.run(function($rootScope) {
+var constants = {
+    db:{ 
+        folders:{
+            users:"memoru/users",
+            lists: "lists"
+        }
+    },
+    test:{
+       userID: "test-id"
+    }
+};
+
+/* Run function is executed after .config, and before .controller */
+memoruAngular.run(function($rootScope) {
 	$rootScope.appConstants = {
-        app:{name:"Memoru", version: "1.0"}
+        app:{name:"Memoru", version: "1.0"},
     };
 
     $rootScope.i18n = {
         menu:{login:"Log in", logout: "Log out", register:"Register"},
         labels:{tasks:"Tasks"}
     };
+
+    memoruFire.database().ref().child("memoru/users/test-id/auth").once('value').then(
+		function(snapshot){
+			if(snapshot.val()){
+                console.debug("Got configurations from Firebase");
+                console.log(snapshot.val());
+			}else{
+				console.error("Configurations not available in Firebase");
+			}
+	});
 
 });
