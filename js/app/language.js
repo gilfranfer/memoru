14f9c04317d6008dec66c2312d26062d88ef2905
Francisco Gil <franfer@Franfers-MacPro.attlocal.net>
2020-10-19 09:36:22 -0500

    Form for list creation

--- js/app/language.js
+++ js/app/language.js
@@ -1,10 +1,13 @@
 const languages = {
     english: {
         menu:{login:"Log in", logout: "Log out", register:"Register"},
-        labels:{tasks:"Tasks"}
+        labels:{tasks:"Tasks"},
+        lists:{addTitle:"Manage your Lists", addHint:"Type the list name", addDesc:"Use 3 and up to 25 letters"}
     },
     spanish: {
         menu:{login:"Iniciar sesión", logout: "Cerrar sesión", register:"Registro"},
-        labels:{tasks:"Tareas"}
+        labels:{tasks:"Tareas"},
+        lists:{addTitle:"Administra tus Listas", addHint:"Escribe el nombre de la lista", 
+                addDesc:"Utiliza de 3 a 25 letras"}
     }
 };
